8643b02f7db57a3fe26f5ebff1c412f6838f13b0
Kristina0805 <kristya25.95@mail.ru>
2022-11-30 16:45:48 +0500

    new test

--- src/test/java/ru/netology/testmode/test/TestMode.java
+++ src/test/java/ru/netology/testmode/test/TestMode.java
@@ -1,4 +1,38 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+
 public class TestMode {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        //TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
+        //данными зарегистрированного активного пользователя, для заполнения полей формы используйте
+        //пользователя registeredUser
+
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+
+        $("[id='root']").shouldHave(Condition.text("Личный кабинет"), Duration.ofSeconds(15));
+    }
+
+
 }
